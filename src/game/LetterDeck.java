3046524ed1bcf8d284c5f397351774cdeacf152d
Tlin8369 <tytenn5@gmail.com>
2022-03-28 20:55:33 -0400

    Added things to tiles deck and tile

--- src/game/LetterDeck.java
+++ src/game/LetterDeck.java
@@ -1,5 +1,25 @@
 package game;
 
-public class LetterDeck {
+import java.util.ArrayList;
 
+public class LetterDeck {
+	private ArrayList<String> currentTiles;
+
+	LetterDeck() {
+		//Randomized but weighted towards common letters
+	}
+
+	public void fill() {
+		//If currentTiles is less than 7
+		//then fill the arrayList till it hits 7
+		//Potentially use same random as in constructor,
+		//Might have to use different one to avoid repeated
+		//character overlap in reserve
+	}
+
+	public void remove() {
+		//Remove tiles from currentTiles
+		//Probably need a parameter or something to detect
+		//what needs to be removed
+	}
 }
--- src/game/LetterTile.java
+++ src/game/LetterTile.java
@@ -1,5 +1,26 @@
 package game;
 
 public class LetterTile {
-
+	private char assignedLetter;
+	/*Boolean is for the sake of placing before confirming,
+	 not telling whether or not it is in reserve or on the board*/
+	private boolean placed;
+	LetterTile(char letter, boolean random, boolean biased) {
+		assignedLetter = letter;
+		placed = false;
+	}
+	
+	//Random letter with bias towards more common
+	private biasedRandom() {
+		
+	}
+	
+	//Pure random as the name entails
+	private pureRandom() {
+		
+	}
+	
+	public Position getPosition() {
+		//Retrieves position
+	}
 }
