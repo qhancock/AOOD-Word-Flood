415434431dd1dafb47d1b3b04cb1b8e804875d1f
Quinn Hancock <90471837+qhancock@users.noreply.github.com>
2022-03-30 08:40:37 -0400

    Initial constructors + stuff with random letters

--- src/game/LetterTile.java
+++ src/game/LetterTile.java
@@ -1,5 +1,17 @@
 package game;
 
 public class LetterTile {
-	private char letter;	
+	private char letter;
+	
+	public LetterTile() {
+		this.letter = data.LetterFactory.weightedRandomLetter();
+	}
+	
+	public LetterTile(char letter) {
+		this.letter = letter;
+	}
+	
+	public char getLetter() {
+		return letter;
+	}
 }
