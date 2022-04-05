e6a6c0b59896c1ee38e53452332a057062274c1a
Tlin8369 <tytenn5@gmail.com>
2022-04-05 08:25:46 -0400

    changed variable

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,7 +5,7 @@ public class Game {
 	//Take score, need a getter for board
 	public int score() {
 		int score = 0;
-		Board grid = getBoard();
+		Lettertile[][] grid = getBoard();
 		for (int x = 0; x < 250; x++) {
 			for (int y = 0; y < 250; y++) {
 				if (grid[y][x] != null) {
