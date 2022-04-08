755ff1a521d31df663f4c4648c126d2a70cf3f1e
Tlin8369 <tytenn5@gmail.com>
2022-04-08 08:41:14 -0400

    Added timer and working on information exchange between deck and grid

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,18 +1,45 @@
 package game;
+import java.util.*;
+
+import game.Board.Position;
 
 public class Game {
 	private TileDeck deck;
 	private Board grid;
+	private boolean time;
+	private Timer timer = new Timer();
+	
 	public Game() {
 		grid = new Board();
 		deck = new TileDeck();
 	}
 	
+	public void startGame () {
+		time = true;
+		class Task extends TimerTask {
+			public void run() {
+				time = false;
+			}
+		}
+
+		timer.schedule(new Task(), 5, 2*60*1000);
+	}
+	
+	public void placeDeckTile(int index, Position position) {
+		LetterTile placed = deck.getTile(index);
+		Board.Position place = position;
+		place.putTile(placed);
+		
+	}
+
 	public TileDeck getDeck() {
 		return deck;
 	}
-	
+
 	public Board getBoard() {
 		return grid;
 	}
+
+
+
 }
