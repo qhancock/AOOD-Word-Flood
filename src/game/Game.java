f5398eaeb310543fc384a658636037ea2eb61eac
Tlin8369 <tytenn5@gmail.com>
2022-04-08 14:56:41 -0400

    Added swap, place, and confirmation methods

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,12 +5,15 @@ import game.Board.Position;
 
 public class Game {
 	private TileDeck deck;
+	private Board secondaryGrid;
 	private Board grid;
 	private boolean time;
+	private long end;
 	private Timer timer = new Timer();
 	
 	public Game() {
 		grid = new Board();
+		secondaryGrid = new Board();
 		deck = new TileDeck();
 	}
 	
@@ -21,7 +24,7 @@ public class Game {
 				time = false;
 			}
 		}
-
+		end = System.currentTimeMillis() + (2*60*1000);
 		timer.schedule(new Task(), 5, 2*60*1000);
 	}
 	
@@ -29,7 +32,38 @@ public class Game {
 		LetterTile placed = deck.getTile(index);
 		Board.Position place = position;
 		place.putTile(placed);
+		deck.drop(placed);
 		
+	}
+	
+	public void swapDeckTile (int index, Position position) {
+		Board.Position place = position;
+		place.putTile(deck.swap(position.getTile(), index));
+	}
+	
+	public void discard(int index) {
+		deck.drop(deck.getTile(index));
+		deck.fill();
+		timer.cancel();
+		class Task extends TimerTask {
+			public void run() {
+				time = false;
+			}
+		}
+		
+		end = end - 8000;
+		timer.schedule(new Task(), 5, end);
+	}
+	
+	public boolean checkValid() {
+		//Make something to go through and check validity for board
+		secondaryGrid.valid();
+		return true;
+	}
+	
+	public void confirmPlacement() {
+		grid = secondaryGrid;
+		deck.fill();
 	}
 
 	public TileDeck getDeck() {
