b529cdb209908270d88f11d00ec6ff56dbc86aef
Tlin8369 <tytenn5@gmail.com>
2022-04-19 08:44:34 -0400

    Added check valid

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,18 +5,16 @@ import game.Board.Position;
 
 public class Game {
 	private TileDeck deck;
-	private Board secondaryGrid;
 	private Board grid;
 	private boolean time;
 	private long end;
 	private Timer timer = new Timer();
-	
+
 	public Game() {
 		grid = new Board();
-		secondaryGrid = new Board();
 		deck = new TileDeck();
 	}
-	
+
 	public void startGame () {
 		time = true;
 		class Task extends TimerTask {
@@ -27,20 +25,20 @@ public class Game {
 		end = System.currentTimeMillis() + (2*60*1000);
 		timer.schedule(new Task(), 5, 2*60*1000);
 	}
-	
+
 	public void placeDeckTile(int index, Position position) {
 		LetterTile placed = deck.getTile(index);
 		Board.Position place = position;
 		place.putTile(placed);
 		deck.drop(placed);
-		
+
 	}
-	
+
 	public void swapDeckTile (int index, Position position) {
 		Board.Position place = position;
 		place.putTile(deck.swap(position.getTile(), index));
 	}
-	
+
 	public void discard(int index) {
 		deck.drop(deck.getTile(index));
 		deck.fill();
@@ -50,19 +48,23 @@ public class Game {
 				time = false;
 			}
 		}
-		
+
 		end = end - 8000;
 		timer.schedule(new Task(), 5, end);
 	}
-	
+
 	public boolean checkValid() {
-		//Make something to go through and check validity for board
-		secondaryGrid.valid();
+		int size = grid.getTiledPositions().size();
+		for (int x = 0; x < size; x++) {
+			if (!grid.getTiledPositions().get(x).valid()) {
+				return false;
+			}
+		}
+		
 		return true;
 	}
-	
+
 	public void confirmPlacement() {
-		grid = secondaryGrid;
 		deck.fill();
 	}
 
