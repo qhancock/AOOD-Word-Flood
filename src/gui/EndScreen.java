36fb0b3091f1db839e8be79cc1d0fc4ec24a7b94
Tlin8369 <tytenn5@gmail.com>
2022-05-11 08:59:32 -0400

    Constructors for screens in progress

--- src/gui/EndScreen.java
+++ src/gui/EndScreen.java
@@ -3,5 +3,12 @@ package gui;
 import javax.swing.*;
 
 public class EndScreen extends JPanel {
-
+	JPanel panel;
+	JLabel endScore;
+	JButton retry;
+	JButton exit;
+	public EndScreen(int score) {
+		
+		panel = new JPanel();
+	}
 }
--- src/gui/StartScreen.java
+++ src/gui/StartScreen.java
@@ -3,5 +3,11 @@ package gui;
 import javax.swing.*;
 
 public class StartScreen extends JPanel{
-
+	JPanel panel;
+	JButton startButton;
+	JLabel instructions;
+	public StartScreen() {
+		panel = new JPanel();
+		
+	}
 }
