1d7b168191b97127f7759e0eeee7efb687abb905
Quinn Hancock <90471837+qhancock@users.noreply.github.com>
2022-05-17 02:10:03 -0400

    finalized Game for interaction with front-end and other logic constructs

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,69 +1,83 @@
 package game;
-import java.util.*;
 
-import game.Board.Position;
+import gui.GameWindow;
 
 public class Game {
-	//delete or comment out timer portion later
+	public GameWindow window = null;
+	
 	private TileDeck deck;
 	private Board board;
+	
+	private int score;
 
 	public Game() {
 		board = new Board();
+		board.game = this;
 		deck = new TileDeck();
+		deck.game = this;
 		deck.fill();
-		
-	}
-
-	public void placeDeckTile(int index, Position position) {
-		//Places a tile on the board
-		LetterTile placed = deck.getTile(index);
-		Board.Position place = board.new Position(position.row(), position.col());
-		place.putTile(placed);
-		deck.drop(placed);
-
 	}
 	
-	public void swapGridTile (Position position1, Position position2) {
-		//Swaps two grid tiles with each other
-		LetterTile hold = position1.getTile();
-		Board.Position place1 = board.new Position(position1.row(), position1.col());
-		Board.Position place2 = board.new Position(position2.row(), position2.col());
-		place1.putTile(position2.getTile());
-		place2.putTile(hold);
+	int selectedDeckIndex = -1;
+	Board.Position selectedBoardPosition = null;
+	
+	public void deckSelect(int index) {
+		
+		if(selectedBoardPosition == null) {
+			deck.select(index);
+			selectedDeckIndex = deck.getSelectedIndex();
+		} else {
+			doSelectionInterchange(index, selectedBoardPosition);
+		}
+		this.window.updateConfirmPanel(this.checkValid());
+	}
+	
+	public void boardSelect(Board.Position newSelectedPosition) {
+
+		//nothing selected in the deck
+		if(selectedDeckIndex == -1) {
+			board.select(newSelectedPosition);
+			selectedBoardPosition = board.getSelectedPosition();
+		} else {
+			doSelectionInterchange(selectedDeckIndex, newSelectedPosition);
+		}
+		this.window.updateConfirmPanel(this.checkValid());
+		
+	}
+	
+	private void doSelectionInterchange(int deckIndex, Board.Position position) {
+		
+		LetterTile positionTile = position.getTile();
+		LetterTile indexTile = deck.getTile(deckIndex);
+		
+		board.deselect();
+		deck.deselect();
+		
+		tileInterchange(deckIndex, position);
+		selectedDeckIndex = deck.getSelectedIndex();
+		selectedBoardPosition = board.getSelectedPosition();
+		
+		window.updateAll();
+	}
+	
+	/*
+	 * facilitates the transfer of a tile 
+	 * from the deck at an index to the
+	 * board at a position or vice versa
+	 */
+	public void tileInterchange(int deckIndex, Board.Position position) {
+		deck.putTile(position.putTile(deck.getTile(deckIndex)), deckIndex);
 	}
 
-	public void swapDeckTile (int index, Position position) {
-		//Swaps a tile on the board with a tile in the deck
-		Board.Position place = board.new Position(position.row(), position.col());
-		place.putTile(deck.swap(place.getTile(), index));
-	}
-
-	public void discard(int index) {
-		//Drops and replaces a tile in the deck
-		//Need to delete timer portion later
-		deck.drop(deck.getTile(index));
-		deck.fill();
-	}
-
+	//checks if all placed tiles are valid
 	public boolean checkValid() {
-		//checks if all placed tiles are valid
 		int size = board.getTiledPositions().size();
 		for (int x = 0; x < size; x++) {
 			if (!board.getTiledPositions().get(x).valid()) {
 				return false;
 			}
 		}
-		
 		return true;
-	}
-
-	public void confirmPlacement() {
-		//Run when confirm button is pressed
-		//Make a repaint/popup appear if its false
-		if (checkValid()) {
-			deck.fill();
-		}
 	}
 
 	public TileDeck getDeck() {
@@ -74,5 +88,19 @@ public class Game {
 	public Board getBoard() {
 		//Retrieves the board
 		return board;
+	}
+	
+	public void refillBar() {
+		if(this.checkValid())
+		setScore(getScore()+ this.deck.fill());
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+		this.window.updateScorePanel(this.score);
+	}
+	
+	public int getScore() {
+		return this.score;
 	}
 }
